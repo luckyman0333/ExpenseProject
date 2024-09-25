ae9acc21e4cfbb514ed29964659341c261934fa7
luckyman0333 <82233343+luckyman0333@users.noreply.github.com>
2024-09-25 20:41:19 +0200

    error correction

--- expense_manager/expense_manager/settings.py
+++ expense_manager/expense_manager/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'expense_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
