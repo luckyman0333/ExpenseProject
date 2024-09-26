3f2d692cd1e1746f611cf3ed54bf2f3c025a67af
luckyman0333 <82233343+luckyman0333@users.noreply.github.com>
2024-09-26 20:32:52 +0200

    gb-3 error correction

--- expense_manager/pythonScripts/import_xlsx.py
+++ expense_manager/pythonScripts/import_xlsx.py
@@ -7,18 +7,18 @@ def read_excel(file_path):
         # Select the active sheet
         sheet = workbook.active
 
-        print(f"Виведення даних з файлу: {file_path}\n")
+        print(f"Outputting data from a file: {file_path}\n")
 
         # Row iteration and data output
         for row in sheet.iter_rows(values_only=True):
             print(row)
 
     except Exception as e:
-        print(f"Не вдалося відкрити файл. Помилка: {str(e)}")
+        print(f"Failed to open file. Error: {str(e)}")
 
 
 if __name__ == "__main__":
     
-    file_path = input("Введіть шлях до Excel файлу: ")
+    file_path = input("Enter the path to the Excel file: ")
     
     read_excel(file_path)
